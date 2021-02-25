f3005d12113e505fec21843ee99952e3f169ed27
doNdanSa <78148091+doNdanSa@users.noreply.github.com>
2021-02-25 14:44:31 +0100

    Initial commit

---
+++ Ćwiczenia/Page Visits Funnel/script.py
@@ -0,0 +1,55 @@
+﻿import codecademylib
+import pandas as pd
+
+visits = pd.read_csv('visits.csv',
+parse_dates=[1])
+cart = pd.read_csv('cart.csv',
+parse_dates=[1])
+checkout = pd.read_csv('checkout.csv',
+parse_dates=[1])
+purchase = pd.read_csv('purchase.csv',
+parse_dates=[1])
+
+print(visits.head())
+print(cart.head())
+print(checkout.head())
+print(purchase.head())
+
+v_cart = pd.merge(visits, cart, how='left')
+v_cart_len = len(v_cart)
+#print(v_cart_len)
+
+v_cart_null = v_cart[v_cart.cart_time.isnull()]
+#print(len(v_cart_null))
+v_cart_null_len = len(v_cart_null)
+
+print(float(v_cart_null_len)/v_cart_len)
+
+######################################
+
+c_cart = pd.merge(cart, checkout, how='left')
+c_cart_len = len(c_cart)
+#print(c_cart_len)
+# print(c_cart)
+
+c_cart_null = c_cart[c_cart.checkout_time.isnull()]
+#print(len(c_cart_null))
+c_cart_null_len = len(c_cart_null)
+
+print(float(c_cart_null_len)/c_cart_len)
+
+checkout_purchase = pd.merge(checkout, purchase, how='left')
+checkout_purchase_rows = len(checkout_purchase)
+checkout_purchase_null = len(checkout_purchase[checkout_purchase.purchase_time.isnull()])
+print(float(checkout_purchase_null)/ checkout_purchase_rows)
+
+all_data = visits.merge(cart, how='left').merge(checkout, how='left').merge(purchase, how='left')
+#print(all_data.head())
+
+all_data['time_to_purchase'] = \
+all_data.purchase_time - \
+all_data.visit_time
+
+print(all_data.time_to_purchase)
+print(all_data.time_to_purchase.mean())
+
---
+++ Ćwiczenia/ShoeFly.com/script.py
@@ -0,0 +1,53 @@
+﻿ import pandas as pd
+
+ad_clicks = pd.read_csv('ad_clicks.csv')
+
+print(ad_clicks.head())
+
+clicks = ad_clicks.groupby('utm_source').user_id.count().reset_index()
+
+ad_clicks['is_click'] = ~ad_clicks.ad_click_timestamp.isnull()
+
+clicks_by_source = ad_clicks.groupby(['utm_source', 'is_click']).user_id.count().reset_index()
+
+clicks_pivot = clicks_by_source.pivot(
+columns = 'is_click',
+index = 'utm_source',
+values = 'user_id').reset_index()
+
+print(clicks_pivot)
+
+clicks_pivot['percent-clicked'] = clicks_pivot[True] / (clicks_pivot[True] + clicks_pivot[False])
+
+print(clicks_pivot)
+
+ad_clicks.groupby('experimental_group').user_id.count().reset_index()
+
+numbers = ad_clicks.groupby(['experimental_group', 'is_click']).user_id.count().reset_index()
+number_pivot = numbers.pivot(
+columns = 'is_click',
+index = 'experimental_group',
+values = 'user_id').reset_index()
+
+print(number_pivot)
+
+a_clicks = ad_clicks[ad_clicks.experimental_group == 'A']
+b_clicks = ad_clicks[ad_clicks.experimental_group == 'B']
+
+print(a_clicks)
+
+aa_clk = a_clicks.groupby(['is_click', 'day']).user_id.count().reset_index()
+a_clicks_pivot = aa_clk.pivot(
+columns = 'is_click',
+index = 'day',
+values = 'user_id').reset_index()
+
+bb_clk = b_clicks.groupby(['is_click', 'day']).user_id.count().reset_index()
+b_clicks_pivot = bb_clk.pivot(
+columns = 'is_click',
+index = 'day',
+values = 'user_id').reset_index()
+
+print(a_clicks_pivot)
+print(b_clicks_pivot)
+
